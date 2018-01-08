42943df7b2df2089de36429513f5e889729f5882
loziniakowna <loziniakowna@wp.pl>
2018-01-08 17:01:22 +0100

    added eventListener for window resizing

--- scripts/index.js
+++ scripts/index.js
@@ -5,22 +5,27 @@
 		input = document.querySelector(".c-nav__input"),
 		menu = document.querySelector("#menu"),
 		navMenu = document.querySelector(".c-nav__menu");
-		form = document.querySelector("#form");
-
-
+		form = document.querySelector("#form");	
 
 	search.addEventListener("click", () => {
 		h1.classList.toggle("c-h1--left")
 		input.classList.toggle("is-displayed")
 		search.classList.toggle("o-nav__img--right")
-	}, false)
+	}, false);
 
 	menu.addEventListener("click", () => {
 		navMenu.classList.toggle("is-displayed--flex")
-	}, false)
+	}, false);
 
 	search.addEventListener("click", () => {
-		document.getElementById("form").reset();
-	})
+		form.reset();
+	}, false);
 
+	window.addEventListener("resize", () => {
+		if (this.innerWidth < 800 && input.classList.contains("is-displayed")) {
+			h1.classList.remove("c-h1--left");
+		} else if (input.classList.contains("is-displayed")) {
+			h1.classList.add("c-h1--left");
+		}
+	}, false);
 }
